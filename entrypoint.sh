f2319a04d12b40537b1554c40e94467d0e556f25
Beau Rudder <beau.j.rudder@gmail.com>
2021-08-17 15:14:16 +0100

    use env vars

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 PR_ID=$(echo $GITHUB_REF | tr -dc '0-9')
-echo "$1/pull/$PR_ID"
-printenv
-gogitops -user=github-actions -token=$2 -pullrequest="$1/pull/$PR_ID"
+echo "$GITHUB_REPOSITORY/pull/$PR_ID"
+#printenv
+gogitops -user=github-actions -token=$1 -pullrequest="$GITHUB_REPOSITORY/pull/$PR_ID"
