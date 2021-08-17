e712738fff6752ed0deda84395a618d06b4e462f
Beau Rudder <beau.j.rudder@gmail.com>
2021-08-17 17:56:21 +0100

    Clean out CRUD

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM beaujr/gogitops-action:base
-COPY entrypoint.sh /
-RUN apk add --no-cache bash
-RUN ln -s $(pwd)/gogitops /bin
-ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-PR_ID=$(echo $GITHUB_REF | tr -dc '0-9')
-if [[ -z "${PR_ID}" ]]; then
-  gogitops -user=github-actions -token=$1 -repo="github.com/$GITHUB_REPOSITORY" -sm=true
-else
-  gogitops -user=github-actions -token=$1 -pullrequest="github.com/$GITHUB_REPOSITORY/pull/$PR_ID"
-fi
