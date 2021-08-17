b6d94aa2850e1ddcbdcae06e76a5affb787c8d0b
Beau Rudder <beau.j.rudder@gmail.com>
2021-08-17 14:51:51 +0100

    v3.1

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
 FROM beaujr/gogitops:latest
 COPY entrypoint.sh .
+RUN ln -s $(pwd)/gogitops /bin
 ENTRYPOINT ["./entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-echo "hello"
-./gogitops -user=github-actions -token=$2 -pullrequest="$1/pull/$3"
+PR_ID=$(echo $GITHUB_REF | tr -dc '0-9')
+./gogitops -user=github-actions -token=$2 -pullrequest="$1/pull/$PR_ID"
