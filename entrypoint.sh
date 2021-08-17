713f9b59ca5e5cc13afbf6bd3d10b14e37853abb
Beau Rudder <beau.j.rudder@gmail.com>
2021-08-17 15:35:51 +0100

    review args

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PR_ID=$(echo $GITHUB_REF | tr -dc '0-9')
 if [[ -z "${PR_ID}" ]]; then
-  gogitops -user=github-actions -token=$1 -pullrequest="github.com/$GITHUB_REPOSITORY" -sm=true
+  echo gogitops -user=github-actions -token=$1 -pullrequest="github.com/$GITHUB_REPOSITORY" -sm=true
 else
-  gogitops -user=github-actions -token=$1 -pullrequest="github.com/$GITHUB_REPOSITORY/pull/$PR_ID"
+  echo gogitops -user=github-actions -token=$1 -pullrequest="github.com/$GITHUB_REPOSITORY/pull/$PR_ID"
 fi
